b32eeeac950ad944b03186d2951bfc5d71b5f615
ReactNativeAmalgum <137068385+ReactNativeAmalgum@users.noreply.github.com>
2024-06-27 15:58:30 +0530

    AsyncStorage added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import HistoryPage from './components/HistoryPage';
 import CartPage from './components/CartPage';
+import AsyncStorage, { useAsyncStorage } from '@react-native-async-storage/async-storage';
 
 const App = () => {
   const [history, setHistory] = useState([]);
   const [cart, setCart] = useState([]);
-
-  const addToHistory = (imageUrl) => {
-    setHistory((prevHistory) => [...prevHistory, imageUrl]);
+  
+  useEffect(() =>{
+    const loadHistory = () =>{
+      const storedHistory = localStorage.getItem('imageHistory');
+      if(storedHistory){
+        setHistory(JSON.parse(storedHistory))
+      }
+    }
+    loadHistory()
+  },[])
+  const addToHistory = async (imageUrl) => {
+    try {
+      const updatedHistory = [imageUrl, ...history];
+      setHistory(updatedHistory);
+      await AsyncStorage.setItem('imageHistory',  JSON.stringify(updatedHistory));
+    } catch (error) {
+      console.log("Error while saving images".error)
+    }
   };
 
   const addToCart = (item) => {
